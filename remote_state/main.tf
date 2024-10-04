4ede18e2f0aea14650a9eb72908c555a1f8a9063
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2024-10-04 16:57:19 +0800

    fix pipeline

--- remote_state/main.tf
+++ remote_state/main.tf
@@ -1,28 +1,32 @@
-
 resource "aws_s3_bucket" "bharath_tf_state_store" {
   bucket = "bharaths-eu-terraform-remote-state"
 
-  # Prevent Accedental Delete of this bucket
+  # Prevent Accidental Delete of this bucket
   lifecycle {
     prevent_destroy = true
   }
+
   # Enables versioning
   versioning {
     enabled = true
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bharath_tf_state_store_sse" {
+  bucket = aws_s3_bucket.bharath_tf_state_store.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
 
 resource "aws_dynamodb_table" "bharaths-terraform-locks" {
   hash_key = "LockID"
-  name = "bharaths-eu-terraform-remote-state-locks"
+  name     = "bharaths-eu-terraform-remote-state-locks"
   billing_mode = "PAY_PER_REQUEST"
+
   attribute {
     name = "LockID"
     type = "S"
@@ -30,11 +34,11 @@ resource "aws_dynamodb_table" "bharaths-terraform-locks" {
 }
 
 output "bharath_s3_backend_arn" {
-  value = aws_s3_bucket.bharath_tf_state_store.arn
+  value       = aws_s3_bucket.bharath_tf_state_store.arn
   description = "The S3 Bucket ARN"
 }
 
 output "bharath_dynamodb_table_name" {
-  value = aws_dynamodb_table.bharaths-terraform-locks.name
-  description = "The Name of the Dynamo DB table"
+  value       = aws_dynamodb_table.bharaths-terraform-locks.name
+  description = "The Name of the DynamoDB table"
 }
