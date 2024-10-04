aae4fe05d67010272097a45abb43026494f16254
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2024-10-04 17:08:30 +0800

    create vpc in eu-west-2

--- EKS_Private_Cluster/vpc/backend.tf
+++ EKS_Private_Cluster/vpc/backend.tf
@@ -1,9 +1,9 @@
 
 terraform {
   backend "s3" {
-    bucket = "bharaths-test-terraform-remote-state"
+    bucket = "bharaths-eu-terraform-remote-state"
     key = "vpc"
-    dynamodb_table = "bharaths-test-terraform-remote-state-locks"
+    dynamodb_table = "bharaths-eu-terraform-remote-state-locks"
     region = "eu-west-2"
   }
 }
