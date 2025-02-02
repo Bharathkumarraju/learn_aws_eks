52b2f91cf8fa8dfa3b61b60acf6b7000867e6601
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-02-02 09:36:48 +0800

    node exporter and kube-state-metrics

--- 10.EKS-prom-stack/2.install-kube-prometheus-stack.sh
+++ 10.EKS-prom-stack/2.install-kube-prometheus-stack.sh
@@ -28,3 +28,105 @@ monitoring-prometheus-node-exporter-qrdn8             1/1     Running   0       
 bharathkumardasaraju@10.EKS-prom-stack$ 
 
 
+bharathkumardasaraju@learn_aws_eks$ kubectl get svc -n monitoring
+NAME                                      TYPE        CLUSTER-IP       EXTERNAL-IP   PORT(S)                      AGE
+alertmanager-operated                     ClusterIP   None             <none>        9093/TCP,9094/TCP,9094/UDP   14m
+monitoring-grafana                        ClusterIP   10.100.60.244    <none>        80/TCP                       14m
+monitoring-kube-prometheus-alertmanager   ClusterIP   10.100.91.134    <none>        9093/TCP,8080/TCP            14m
+monitoring-kube-prometheus-operator       ClusterIP   10.100.45.202    <none>        443/TCP                      14m
+monitoring-kube-prometheus-prometheus     ClusterIP   10.100.55.140    <none>        9090/TCP,8080/TCP            14m
+monitoring-kube-state-metrics             ClusterIP   10.100.162.202   <none>        8080/TCP                     14m
+monitoring-prometheus-node-exporter       ClusterIP   10.100.207.124   <none>        9100/TCP                     14m
+prometheus-operated                       ClusterIP   None             <none>        9090/TCP                     14m
+bharathkumardasaraju@learn_aws_eks$ 
+
+
+
+bharathkumardasaraju@learn_aws_eks$ kubectl get pods -n monitoring
+NAME                                                     READY   STATUS    RESTARTS   AGE
+alertmanager-monitoring-kube-prometheus-alertmanager-0   2/2     Running   0          14m
+alertmanager-monitoring-kube-prometheus-alertmanager-1   2/2     Running   0          14m
+monitoring-grafana-5bc784d8dd-447kh                      3/3     Running   0          14m
+monitoring-kube-prometheus-operator-bf8679fbb-tcz8h      1/1     Running   0          14m
+monitoring-kube-state-metrics-545974fbd4-zbmzp           1/1     Running   0          14m
+monitoring-prometheus-node-exporter-pn7jt                1/1     Running   0          14m
+monitoring-prometheus-node-exporter-qrdn8                1/1     Running   0          14m
+prometheus-monitoring-kube-prometheus-prometheus-0       2/2     Running   0          14m
+bharathkumardasaraju@learn_aws_eks$ 
+
+
+
+bharathkumardasaraju@learn_aws_eks$ kubectl port-forward service/prometheus-operated -n monitoring 9090:9090
+Forwarding from 127.0.0.1:9090 -> 9090
+Forwarding from [::1]:9090 -> 9090
+Handling connection for 9090
+Handling connection for 9090
+Handling connection for 9090
+Handling connection for 9090
+Handling connection for 9090
+
+
+bharathkumardasaraju@learn_aws_eks$ kubectl port-forward service/monitoring-grafana -n monitoring 8080:80
+Forwarding from 127.0.0.1:8080 -> 3000
+Forwarding from [::1]:8080 -> 3000
+
+
+bharathkumardasaraju@learn_aws_eks$ kubectl port-forward service/alertmanager-operated -n monitoring 9093:9093
+Forwarding from 127.0.0.1:9093 -> 9093
+Forwarding from [::1]:9093 -> 9093
+
+
+
+
+bharathkumardasaraju@learn_aws_eks$ kubectl get servicemonitors.monitoring.coreos.com  -n monitoring
+NAME                                                 AGE
+monitoring-grafana                                   25m
+monitoring-kube-prometheus-alertmanager              25m
+monitoring-kube-prometheus-apiserver                 25m
+monitoring-kube-prometheus-coredns                   25m
+monitoring-kube-prometheus-kube-controller-manager   25m
+monitoring-kube-prometheus-kube-etcd                 25m
+monitoring-kube-prometheus-kube-proxy                25m
+monitoring-kube-prometheus-kube-scheduler            25m
+monitoring-kube-prometheus-kubelet                   25m
+monitoring-kube-prometheus-operator                  25m
+monitoring-kube-prometheus-prometheus                25m
+monitoring-kube-state-metrics                        25m
+monitoring-prometheus-node-exporter                  25m
+bharathkumardasaraju@learn_aws_eks$ kubectl get servicemonitors.monitoring.coreos.com monitoring-grafana  -n monitoring -o
+ yaml
+apiVersion: monitoring.coreos.com/v1
+kind: ServiceMonitor
+metadata:
+  annotations:
+    meta.helm.sh/release-name: monitoring
+    meta.helm.sh/release-namespace: monitoring
+  creationTimestamp: "2025-02-02T00:12:16Z"
+  generation: 1
+  labels:
+    app.kubernetes.io/instance: monitoring
+    app.kubernetes.io/managed-by: Helm
+    app.kubernetes.io/name: grafana
+    app.kubernetes.io/version: 11.3.0
+    helm.sh/chart: grafana-8.5.12
+  name: monitoring-grafana
+  namespace: monitoring
+  resourceVersion: "7588"
+  uid: 6f535501-3e37-4cb5-82c4-536f09df8011
+spec:
+  endpoints:
+  - honorLabels: true
+    path: /metrics
+    port: http-web
+    scheme: http
+    scrapeTimeout: 30s
+  jobLabel: monitoring
+  namespaceSelector:
+    matchNames:
+    - monitoring
+  selector:
+    matchLabels:
+      app.kubernetes.io/instance: monitoring
+      app.kubernetes.io/name: grafana
+bharathkumardasaraju@learn_aws_eks$ 
+
---
+++ 10.EKS-prom-stack/3.k8s-node-exporter.sh
@@ -0,0 +1,121 @@
+bharathkumardasaraju@learn_aws_eks$ kubectl get svc -n monitoring -o wide| grep -i node
+monitoring-prometheus-node-exporter       ClusterIP   10.100.207.124   <none>        9100/TCP                     81m   app.kubernetes.io/instance=monitoring,app.kubernetes.io/name=prometheus-node-exporter
+bharathkumardasaraju@learn_aws_eks$ kubectl get svc -n monitoring -o wide| grep -i kube-state
+monitoring-kube-state-metrics             ClusterIP   10.100.162.202   <none>        8080/TCP                     81m   app.kubernetes.io/instance=monitoring,app.kubernetes.io/name=kube-state-metrics
+bharathkumardasaraju@learn_aws_eks$ 
+
+
+root@i-0df208b264c59a88b bin]# curl 10.100.207.124:9100/metrics
+# HELP go_gc_duration_seconds A summary of the pause duration of garbage collection cycles.
+# TYPE go_gc_duration_seconds summary
+go_gc_duration_seconds{quantile="0"} 1.7153e-05
+go_gc_duration_seconds{quantile="0.25"} 3.9567e-05
+go_gc_duration_seconds{quantile="0.5"} 4.4767e-05
+go_gc_duration_seconds{quantile="0.75"} 5.6816e-05
+go_gc_duration_seconds{quantile="1"} 0.001738216
+go_gc_duration_seconds_sum 0.012789747
+go_gc_duration_seconds_count 151
+# HELP go_goroutines Number of goroutines that currently exist.
+# TYPE go_goroutines gauge
+go_goroutines 8
+# HELP go_info Information about the Go environment.
+# TYPE go_info gauge
+go_info{version="go1.22.5"} 1
+# HELP go_memstats_alloc_bytes Number of bytes allocated and still in use.
+# TYPE go_memstats_alloc_bytes gauge
+go_memstats_alloc_bytes 2.786472e+06
+# HELP go_memstats_alloc_bytes_total Total number of bytes allocated, even if freed.
+# TYPE go_memstats_alloc_bytes_total counter
+go_memstats_alloc_bytes_total 2.75234456e+08
+# HELP go_memstats_buck_hash_sys_bytes Number of bytes used by the profiling bucket hash table.
+# TYPE go_memstats_buck_hash_sys_bytes gauge
+go_memstats_buck_hash_sys_bytes 1.514372e+06
+# HELP go_memstats_frees_total Total number of frees.
+# TYPE go_memstats_frees_total counter
+go_memstats_frees_total 3.481327e+06
+# HELP go_memstats_gc_sys_bytes Number of bytes used for garbage collection system metadata.
+# TYPE go_memstats_gc_sys_bytes gauge
+go_memstats_gc_sys_bytes 3.270432e+06
+# HELP go_memstats_heap_alloc_bytes Number of heap bytes allocated and still in use.
+# TYPE go_memstats_heap_alloc_bytes gauge
+go_memstats_heap_alloc_bytes 2.786472e+06
+# HELP go_memstats_heap_idle_bytes Number of heap bytes waiting to be used.
+# TYPE go_memstats_heap_idle_bytes gauge
+go_memstats_heap_idle_bytes 3.817472e+06
+# HELP go_memstats_heap_inuse_bytes Number of heap bytes that are in use.
+# TYPE go_memstats_heap_inuse_bytes gauge
+go_memstats_heap_inuse_bytes 4.128768e+06
+# HELP go_memstats_heap_objects Number of allocated objects.
+# TYPE go_memstats_heap_objects gauge
+go_memstats_heap_objects 21238
+root@i-0df208b264c59a88b bin]#
+
+root@i-0df208b264c59a88b bin]# curl 10.100.162.202:8080/metrics
+# HELP kube_certificatesigningrequest_annotations Kubernetes annotations converted to Prometheus labels.
+# TYPE kube_certificatesigningrequest_annotations gauge
+# HELP kube_certificatesigningrequest_labels [STABLE] Kubernetes labels converted to Prometheus labels.
+# TYPE kube_certificatesigningrequest_labels gauge
+# HELP kube_certificatesigningrequest_created [STABLE] Unix creation timestamp
+# TYPE kube_certificatesigningrequest_created gauge
+kube_certificatesigningrequest_created{certificatesigningrequest="csr-l9f4h",signer_name="kubernetes.io/kubelet-serving"} 1.738453553e+09
+kube_certificatesigningrequest_created{certificatesigningrequest="csr-xc2t5",signer_name="kubernetes.io/kubelet-serving"} 1.738453553e+09
+# HELP kube_certificatesigningrequest_condition [STABLE] The number of each certificatesigningrequest condition
+# TYPE kube_certificatesigningrequest_condition gauge
+kube_certificatesigningrequest_condition{certificatesigningrequest="csr-l9f4h",signer_name="kubernetes.io/kubelet-serving",condition="approved"} 1
+kube_certificatesigningrequest_condition{certificatesigningrequest="csr-l9f4h",signer_name="kubernetes.io/kubelet-serving",condition="denied"} 0
+kube_certificatesigningrequest_condition{certificatesigningrequest="csr-xc2t5",signer_name="kubernetes.io/kubelet-serving",condition="approved"} 1
+kube_certificatesigningrequest_condition{certificatesigningrequest="csr-xc2t5",signer_name="kubernetes.io/kubelet-serving",condition="denied"} 0
+# HELP kube_certificatesigningrequest_cert_length [STABLE] Length of the issued cert
+# TYPE kube_certificatesigningrequest_cert_length gauge
+kube_certificatesigningrequest_cert_length{certificatesigningrequest="csr-l9f4h",signer_name="kubernetes.io/kubelet-serving"} 1074
+kube_certificatesigningrequest_cert_length{certificatesigningrequest="csr-xc2t5",signer_name="kubernetes.io/kubelet-serving"} 1074
+# HELP kube_configmap_annotations Kubernetes annotations converted to Prometheus labels.
+# TYPE kube_configmap_annotations gauge
+# HELP kube_configmap_labels [STABLE] Kubernetes labels converted to Prometheus labels.
+# TYPE kube_configmap_labels gauge
+# HELP kube_configmap_info [STABLE] Information about configmap.
+# TYPE kube_configmap_info gauge
+kube_configmap_info{namespace="monitoring",configmap="monitoring-kube-prometheus-alertmanager-overview"} 1
+kube_configmap_info{namespace="monitoring",configmap="monitoring-kube-prometheus-controller-manager"} 1
+kube_configmap_info{namespace="monitoring",configmap="monitoring-kube-prometheus-k8s-resources-node"} 1
+kube_configmap_info{namespace="monitoring",configmap="monitoring-kube-prometheus-namespace-by-pod"} 1
+kube_configmap_info{namespace="monitoring",configmap="monitoring-kube-prometheus-namespace-by-workload"} 1
+kube_configmap_info{namespace="monitoring",configmap="monitoring-kube-prometheus-proxy"} 1
+kube_configmap_info{namespace="monitoring",configmap="monitoring-kube-prometheus-scheduler"} 1
+kube_configmap_info{namespace="default",configmap="kube-root-ca.crt"} 1
+kube_configmap_info{namespace="kube-system",configmap="aws-auth"} 1
+kube_configmap_info{namespace="monitoring",configmap="monitoring-kube-prometheus-grafana-overview"} 1
+kube_configmap_info{namespace="kube-system",configmap="amazon-vpc-cni"} 1
+kube_configmap_info{namespace="kube-system",configmap="kube-apiserver-legacy-service-account-token-tracking"} 1
+kube_configmap_info{namespace="kube-system",configmap="kube-proxy"} 1
+kube_configmap_info{namespace="kube-system",configmap="kube-root-ca.crt"} 1
+root@i-0df208b264c59a88b bin]#
+
+
+[root@i-0df208b264c59a88b bin]# hostname
+i-0df208b264c59a88b.eu-west-3.compute.internal
+[root@i-0df208b264c59a88b bin]# cat /etc/*release*
+NAME="Amazon Linux"
+VERSION="2"
+ID="amzn"
+ID_LIKE="centos rhel fedora"
+VERSION_ID="2"
+PRETTY_NAME="Amazon Linux 2"
+ANSI_COLOR="0;33"
+CPE_NAME="cpe:2.3:o:amazon:amazon_linux:2"
+HOME_URL="https://amazonlinux.com/"
+SUPPORT_END="2025-06-30"
+Amazon Linux release 2 (Karoo)
+cpe:2.3:o:amazon:amazon_linux:2
+[root@i-0df208b264c59a88b bin]#
+
+
+
+
+
+
+bharathkumardasaraju@learn_aws_eks$ kubectl get svc -n monitoring -o wide| grep -i node
+monitoring-prometheus-node-exporter       ClusterIP   10.100.207.124   <none>        9100/TCP                     81m   app.kubernetes.io/instance=monitoring,app.kubernetes.io/name=prometheus-node-exporter
+bharathkumardasaraju@learn_aws_eks$ kubectl get svc -n monitoring -o wide| grep -i kube-state
+monitoring-kube-state-metrics             ClusterIP   10.100.162.202   <none>        8080/TCP                     81m   app.kubernetes.io/instance=monitoring,app.kubernetes.io/name=kube-state-metrics
+bharathkumardasaraju@learn_aws_eks$ 
